1f6bae96374b85c8a894fb7cf1abb7a5fc063f72
alg <alg.info@gmail.com>
2013-07-08 13:39:02 +0900

    表記カスタマイズ機能の追加

--- icaltenkiemoji.php
+++ icaltenkiemoji.php
@@ -1,6 +1,9 @@
 <?php
 	ini_set('default_charset', 'UTF-8');
 
+	header("Cache-Control: no-cache");
+	header("Content-Type: text/plain; charset=utf-8");
+
 	// デフォルトの表示地域…東京
 	$pref = '13';
 	$area = '63';
@@ -10,6 +13,21 @@
 	}
 	if (isset($_GET['a'])) {
 		$area = htmlspecialchars($_GET['a'], ENT_QUOTES, 'UTF-8');
+	}
+	// 時々とのちの表記パターン
+	$displaypattern = '0';
+	if (isset($_GET['d'])) {
+		$displaypattern = htmlspecialchars($_GET['d'], ENT_QUOTES, 'UTF-8');
+	}
+	// 最高気温を表示しない？
+	$showmaxtemp = '1';
+	if (isset($_GET['m'])) {
+		$showmaxtemp = htmlspecialchars($_GET['m'], ENT_QUOTES, 'UTF-8');
+	}
+	// 最低気温を表示しない？
+	$showmintemp = '1';
+	if (isset($_GET['n'])) {
+		$showmintemp = htmlspecialchars($_GET['n'], ENT_QUOTES, 'UTF-8');
 	}
 	// icalデータの取得
 	$info = file_get_contents("http://weather.livedoor.com/forecast/ical/".$pref."/".$area.".ics");
@@ -23,8 +41,27 @@
 			$info[$i] = str_replace("曇り", "☁", $info[$i]);
 			$info[$i] = str_replace("曇", "☁", $info[$i]);
 			$info[$i] = str_replace("雪", "⛄", $info[$i]);
-			$info[$i] = str_replace("時々", "", $info[$i]);
-			$info[$i] = str_replace("のち", "/", $info[$i]);
+			if ($displaypattern === '0') {
+				$info[$i] = str_replace("時々", "", $info[$i]);
+				$info[$i] = str_replace("のち", "/", $info[$i]);
+			} else {
+				$info[$i] = str_replace("時々", "|", $info[$i]);
+				$info[$i] = str_replace("のち", "→", $info[$i]);
+			}
+			// 最高・最低気温
+			if ($showmaxtemp === '1' && $showmintemp === '1') {
+				// そのまま
+			} elseif ($showmaxtemp === '1') {
+				// 最高だけ
+				$info[$i] = substr($info[$i], 0, strrpos($info[$i], '/'));
+			} elseif ($showmintemp === '1') {
+				// 最低だけ
+				$info[$i] = substr($info[$i], 0, strrpos($info[$i], ' ')+1).
+							substr($info[$i], strrpos($info[$i], '/')+1, strlen($info[$i])-strrpos($info[$i], '/')-1);
+			} else {
+				// 気温なし
+				$info[$i] = substr($info[$i], 0, strrpos($info[$i], ' '));
+			}
 		}
 		print $info[$i]."\n";
 	}
