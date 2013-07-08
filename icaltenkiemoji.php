4b9c55567055e98b63e0e17ae44c70dba6dbfd04
alg <alg.info@gmail.com>
2013-07-08 14:03:39 +0900

    気温の華氏表示オプションを追加

--- icaltenkiemoji.php
+++ icaltenkiemoji.php
@@ -29,6 +29,11 @@
 	if (isset($_GET['n'])) {
 		$showmintemp = htmlspecialchars($_GET['n'], ENT_QUOTES, 'UTF-8');
 	}
+	// 気温を華氏で表示する？
+	$showinfer = '0';
+	if (isset($_GET['f'])) {
+		$showinfer = htmlspecialchars($_GET['f'], ENT_QUOTES, 'UTF-8');
+	}
 	// icalデータの取得
 	$info = file_get_contents("http://weather.livedoor.com/forecast/ical/".$pref."/".$area.".ics");
 	$info = explode("\n", $info);
@@ -36,6 +41,18 @@
 	// SUMMARY行を編集しながら出力
 	for ($i=0; $i<$cnt; $i++) {
 		if (substr($info[$i],0,8) === 'SUMMARY:') {
+			// 最高・最低気温
+			$temps = explode('/', substr($info[$i], strrpos($info[$i], ' ')+1));
+			$tempmax = str_replace("℃", "", $temps[0]);
+			$tempmin = str_replace("℃", "", $temps[1]);
+			if ($showinfer !== '0') {
+				// 気温を華氏で表示
+				$tempmax = round($tempmax * 9 / 5 + 32);
+				$tempmin = round($tempmin * 9 / 5 + 32);
+			}
+			// 気温表記を一旦削除
+			$info[$i] = substr($info[$i], 0, strrpos($info[$i], ' '));
+			// 各種変換
 			$info[$i] = str_replace("晴", "☀", $info[$i]);
 			$info[$i] = str_replace("雨", "☔", $info[$i]);
 			$info[$i] = str_replace("曇り", "☁", $info[$i]);
@@ -48,19 +65,22 @@
 				$info[$i] = str_replace("時々", "|", $info[$i]);
 				$info[$i] = str_replace("のち", "→", $info[$i]);
 			}
-			// 最高・最低気温
 			if ($showmaxtemp === '1' && $showmintemp === '1') {
-				// そのまま
+				// 最高/最低
+				$info[$i].= ' '.$tempmax.'/'.$tempmin;
 			} elseif ($showmaxtemp === '1') {
 				// 最高だけ
-				$info[$i] = substr($info[$i], 0, strrpos($info[$i], '/'));
+				$info[$i].= ' '.$tempmax;
 			} elseif ($showmintemp === '1') {
 				// 最低だけ
-				$info[$i] = substr($info[$i], 0, strrpos($info[$i], ' ')+1).
-							substr($info[$i], strrpos($info[$i], '/')+1, strlen($info[$i])-strrpos($info[$i], '/')-1);
-			} else {
-				// 気温なし
-				$info[$i] = substr($info[$i], 0, strrpos($info[$i], ' '));
+				$info[$i].= ' '.$tempmin;
+			}
+			if ($showmaxtemp === '1' || $showmintemp === '1') {
+				if ($showinfer === '0') {
+					$info[$i].= '℃';
+				} else {
+					$info[$i].= '°F';
+				}
 			}
 		}
 		print $info[$i]."\n";
